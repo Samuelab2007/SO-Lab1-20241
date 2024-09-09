43323044ca8ce1bcb937addd05a157c469ef3f22
Samuelab2007 <samuel.acevedo@udea.edu.co>
2024-09-09 15:26:32 -0500

    Error cuando son demasiados argumentos

--- src/main.c
+++ src/main.c
@@ -1,102 +1,89 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+/*
+Integrantes: Samuel Acevedo Bustamante
 
-/*void read_file(FILE *fd){    
-    int linec = 0;    
-    char line[100];
-    char *stream;
+TODO: Permitir recibir archivos por la stdinput.
+Permitir escoger el nombre del archivo de salida, y también permitir escribir el resultado en
+la salida estándar.
 
-    while(!feof(fd)){
-        stream = fgets(line, 100, fd);
-        linec++;
-    }
+Pa eso capaz tengo que compartimentar el código.
 
-    char *line_list[linec];
-    linec = 0;
-    fseek(fd, 0, SEEK_SET);
-    while(!feof(fd) || !ferror(fd)){  
-        stream = fgets(line, 100, fd); 
-       if (feof(fd) || ferror(fd))  //Amaño para evitar el segmentation fault.
-       {
-        break;
-       }
+*/
 
-        line_list[linec] = strdup(line);
-        linec++;
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    for (int i = linec-1; i >= 0; i--)
+int main(int argc, char *argv[])
+{
+
+    if (argc >= 3)
     {
-        FILE *wptr;
-        wptr = fopen("output.txt","a");
-        fputs(line_list[i] , wptr);
-        fclose(wptr);
-        // Esta joda me está retornando mero archivo binario ahí. Seguramente estoy almacenando mal los strings o algo
+        fprintf(stderr, "Error: Cantidad de argumentos invalida. El máximo de argumentos es 2");
     }
-    
-    
-}*/
 
-
-int main(int argc, char *argv[]){
-
-    //Variable declarations
-    FILE *rptr; //Pointers to files
+    // Variable declarations
+    FILE *rptr; // Pointers to files
     FILE *wptr;
 
-    char *stream;   //Buffers, lines, line_list
+    char *stream; // Buffers, lines, line_list
     char line[100];
     int linec = 0;
-    
-
 
     rptr = fopen(argv[1], "r");
-    
 
     //////////////////
 
     // Contar las líneas del archivo
-    while(!feof(rptr)){
+    while (!feof(rptr))
+    {
         stream = fgets(line, 100, rptr);
         linec++;
     }
 
-    char *line_list[linec]; //Crea un array para guardar las líneas
-    
+    char *line_list[linec]; // Crea un array para guardar las líneas
+
     // Reinicia el contador de líneas y vuelve al inicio del stream.
     linec = 0;
     fseek(rptr, 0, SEEK_SET);
 
-    // Lee el archivo nuevamente, deteniendose en caso de EOF(End of file) o si se presenta algún error. 
-    while(!feof(rptr) || !ferror(rptr)){  
-        stream = fgets(line, 100, rptr); 
-        /*Theres a seg fault because if checks the condition of feof and ferror before it 
+    // Lee el archivo nuevamente, deteniendose en caso de EOF(End of file) o si se presenta algún error.
+    while (!feof(rptr) || !ferror(rptr))
+    {
+        stream = fgets(line, 100, rptr);
+        /*Theres a seg fault because if checks the condition of feof and ferror before it
         reads the next stream line and thus it executes strlen(stream) and it fails
         */
-       if (feof(rptr) || ferror(rptr))  //Amaño para evitar el segmentation fault.
-       {
-        if(ferror(rptr)){
-            fprintf(stderr, "Se presentó un error leyendo el archivo");
-            exit(1);
+        if (feof(rptr) || ferror(rptr)) // Amaño para evitar el segmentation fault.
+        {
+            if (ferror(rptr))
+            {
+                fprintf(stderr, "Se presentó un error leyendo el archivo");
+                exit(1);
+            }
+            break;
         }
-        break;
-       }
         // Clona la línea y almacena el puntero a esta a la tabla line_list
         line_list[linec] = strdup(line);
         linec++;
     }
 
     // Escribe en el archivo de salida.
-    for (int i = linec-1; i >= 0; i--)
+
+    for (int i = linec - 1; i >= 0; i--)
     {
-        wptr = fopen("output.txt", "a");
-        fputs(line_list[i] , wptr);
+        wptr = fopen(argv[2], "a");
+        fputs(line_list[i], wptr);
         fclose(wptr);
         // Esta joda me está retornando mero archivo binario ahí. Seguramente estoy almacenando mal los strings o algo
     }
-    
-    
- 
-}
 
+    // stdin = 0
+    // stdout = 1
+
+    // Usar la variable argc para determinar cuantos argumentos se utilizaron llamando la función.
+    // Si es uno sólo, se hace uso de stdout. Si no son ninguno se lee también stdin.
+    // Si son más de tres. Se manda un error y listo.
+
+    int descriptor = fileno(stdout);
+    printf("Descriptor de la salida estándar: %i", descriptor);
+}
